c063d2f0c5edb870eeea37f9163cd47a2277cf5f
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2025-01-08 19:38:25 +0600

    find store design completed

--- src/components/findStore/FindStore.jsx
+++ src/components/findStore/FindStore.jsx
@@ -5,19 +5,25 @@ import { IoMdSearch } from "react-icons/io";
 const FindStore = () => {
   return (
     <>
-      <div className=" pb-10">
-        <div className="container mx-auto rounded-md max-w-screen-xl bg-gradient-to-r from-blue-300 via-blue-500 to-blue-700">
-          <div className=" py-7 pl-7 pr-5">
-            <div>
-              <FaLocationDot />
+      <div className=" pb-10 px-3">
+        <div className="container rounded-md max-w-screen-xl bg-gradient-to-r from-blue-500 via-blue-700 to-blue-900">
+          <div className=" py-8 pl-7 pr-5 flex flex-col gap-5 items-center md:flex md:justify-between md:items-center md:flex-row">
+            <div className=" md:flex-row md:items-center md:gap-5 flex flex-col items-center gap-2 text-white">
+              <FaLocationDot className=" text-4xl" />
               <div>
-                <h2>20+ Physical Stores</h2>
-                <p>Visit Our Store & Get Your Desired IT Products</p>
+                <h2 className=" text-2xl text-center md:text-3xl font-bold font-titleFont">
+                  20+ Physical Stores
+                </h2>
+                <p className=" text-xs md:text-base font-medium font-bodyFont">
+                  Visit Our Store & Get Your Desired IT Products
+                </p>
               </div>
             </div>
-            <div>
-              <input type="text" placeholder="Find Our Store" />
-              <IoMdSearch />
+            <div className=" w-[180px] md:w-[220px] bg-[#f3a127] py-2 px-5 md:py-4 md:px-10 rounded-full hover:bg-[#a06d20] transition-all duration-200 delay-100 font-medium font-bodyFont">
+              <a href="#" className=" flex items-center gap-2">
+                Find Our Store
+                <IoMdSearch className=" text-xl " />
+              </a>
             </div>
           </div>
         </div>
