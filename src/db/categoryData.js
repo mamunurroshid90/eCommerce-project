606e7773492566a0b057e03ec28d4c4c67dcbf9b
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2025-01-08 13:41:15 +0600

    change featured category icon replace by image png & create feature category data file in db folder

---
+++ src/db/categoryData.js
@@ -0,0 +1,99 @@
+import GeyserHeater from "../assets/category-image/geyser heater.png";
+import Gimbal from "../assets/category-image/gimbal.png";
+import Printer from "../assets/category-image/printer.png";
+import BluetoothSpeaker from "../assets/category-image/bluetooth speaker.png";
+import Camera from "../assets/category-image/camera.png";
+import EarBuds from "../assets/category-image/ear buds.png";
+import GamingConsole from "../assets/category-image/gaming console.png";
+import SSD from "../assets/category-image/SSD.png";
+import Laptop from "../assets/category-image/laptop.png";
+import MobileAccessories from "../assets/category-image/mobile accessories.png";
+import Mobile from "../assets/category-image/mobile.png";
+import Earphone from "../assets/category-image/earphone.png";
+import SmartWatch from "../assets/category-image/smart watch.png";
+import TV from "../assets/category-image/tv.png";
+import VR from "../assets/category-image/VR.png";
+import Drone from "../assets/category-image/drone.png";
+
+export const CategoryData = [
+  {
+    id: 1,
+    image: GeyserHeater,
+    title: "Geyser Heater",
+  },
+  {
+    id: 2,
+    image: Drone,
+    title: "Drone",
+  },
+  {
+    id: 3,
+    image: Gimbal,
+    title: "Gimbal",
+  },
+  {
+    id: 4,
+    image: Printer,
+    title: "Printer Paper",
+  },
+  {
+    id: 5,
+    image: Laptop,
+    title: "Laptop",
+  },
+  {
+    id: 6,
+    image: TV,
+    title: "Smart TV",
+  },
+  {
+    id: 7,
+    image: Mobile,
+    title: "Mobile Phone",
+  },
+  {
+    id: 8,
+    image: MobileAccessories,
+    title: "Mobile Accessories",
+  },
+  {
+    id: 9,
+    image: SSD,
+    title: "Portable SSD",
+  },
+  {
+    id: 10,
+    image: VR,
+    title: "(VR)Virtual Reality",
+  },
+  {
+    id: 11,
+    image: Camera,
+    title: "Action Camera",
+  },
+  {
+    id: 12,
+    image: SmartWatch,
+    title: "Smart Watch",
+  },
+  {
+    id: 13,
+    image: Earphone,
+    title: "Earphone",
+  },
+  {
+    id: 14,
+    image: EarBuds,
+    title: "Ear Buds",
+  },
+  {
+    id: 15,
+    image: BluetoothSpeaker,
+    title: "Bluetooth Speaker",
+  },
+  {
+    id: 16,
+    image: GamingConsole,
+    title: "Gaming Console",
+  },
+];
