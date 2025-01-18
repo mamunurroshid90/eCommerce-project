76dd835df6a513086a1d265cf41fab2c8066671c
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2025-01-18 23:49:28 +0600

    added data in db, some design changes in product cart

--- src/components/featuredProducts/FeaturedProductsCart.jsx
+++ src/components/featuredProducts/FeaturedProductsCart.jsx
@@ -14,15 +14,15 @@ const FeaturedProductsCart = ({
   return (
     <>
       <div>
-        <div className=" bg-white md:h-[300px] h-[250px] lg:p-5 p-3 rounded-md shadow ">
+        <div className=" bg-white max-h-[381px] lg:p-5 p-3 rounded-md shadow ">
           <div className=" max-w-full h-auto aspect-square object-cover flex justify-center items-center overflow-hidden">
             <img src={img} width={180} height={100} alt="image" className=" " />
           </div>
-          <div className=" relative">
-            <h2 className=" text-md font-semibold font-bodyFont tracking-wide">
+          <div className=" flex flex-col gap-2 h-[20%]">
+            <h2 className=" text-cart-title font-semibold font-bodyFont tracking-wide">
               {title}
             </h2>
-            <div className=" absolute -bottom-12 flex items-center gap-4">
+            <div className=" flex items-center gap-4">
               <span className=" text-lg font-bold font-bodyFont text-primaryColor">
                 {newPrice}
               </span>
