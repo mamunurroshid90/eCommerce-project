91685bff182a0e88b183f98d8b6d10869ee58e31
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2025-01-22 16:02:52 +0600

    add more data in product details

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import products from "../../db/data";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -20,7 +20,7 @@ const ProductDetails = () => {
                 <img
                   src={product.img}
                   alt="product img"
-                  className=" h-[150px]"
+                  className=" h-[200px]"
                 />
               </div>
               <div>
@@ -46,6 +46,75 @@ const ProductDetails = () => {
                   <p className=" text-base font-medium font-bodyFont">
                     Features: {product.features}
                   </p>
+                  <Link className=" text-primaryDark border-b border-b-primaryDark inline w-fit capitalize text-base font-bodyFont">
+                    view more info
+                  </Link>
+                  <div className=" mt-4">
+                    <h3 className=" text-xl font-titleFont font-medium">
+                      Payment Options
+                    </h3>
+                    <div className=" grid grid-cols-2 gap-5 mt-3">
+                      <div className=" border py-2 px-3 flex items-center gap-4">
+                        <input
+                          type="radio"
+                          name=""
+                          id=""
+                          className=" w-5 h-5"
+                        />
+                        <div className=" flex flex-col gap-1">
+                          <div className=" flex gap-3 items-center">
+                            <span className=" text-xl font-bold">
+                              {product.newPrice}
+                            </span>
+                            <span className=" text-base font-bold line-through text-[#808080]">
+                              {product.prevPrice}
+                            </span>
+                          </div>
+                          <div className=" flex flex-col">
+                            <span className=" text-sm capitalize">
+                              cash discount price
+                            </span>
+                            <span className=" text-sm  capitalize text-[#838383]">
+                              Online / cash payment
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className=" border py-2 px-3 flex items-center gap-4">
+                        <input
+                          type="radio"
+                          name=""
+                          id=""
+                          className=" w-5 h-5"
+                        />
+                        <div className=" flex flex-col gap-1">
+                          <div className=" flex gap-3 items-center">
+                            <span className=" text-xl font-bold">
+                              $3,358/<span className=" text-sm">month</span>
+                            </span>
+                          </div>
+                          <div className=" flex flex-col">
+                            <span className=" text-sm capitalize">
+                              Regular Price: $40,300
+                            </span>
+                            <span className=" text-sm  capitalize text-[#838383]">
+                              0% EMI for up to 12 Months***
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" flex items-center gap-3">
+                    <div>
+                      <button> - </button>
+                      <button> 1 </button>
+                      <button> + </button>
+                    </div>
+                    <div>
+                      <button>Buy Now</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
