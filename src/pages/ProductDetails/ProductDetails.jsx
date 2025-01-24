0ed12401fd8c10c9d901a48a42ca8761e0718953
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2025-01-24 17:51:46 +0600

    product details pages added some data's

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,8 @@ import products from "../../db/data";
 import { Link, useParams } from "react-router-dom";
 import { FaMinus } from "react-icons/fa6";
 import { FiPlus } from "react-icons/fi";
+import { RiMessage2Fill } from "react-icons/ri";
+import { MdAssignment } from "react-icons/md";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -14,9 +16,10 @@ const ProductDetails = () => {
 
   return (
     <>
-      <div className=" bg-white">
-        <div className=" container max-w-screen-xl mx-auto lg:pt-7 pt-16 h-fit px-3">
+      <div className=" bg-white shadow">
+        <div className=" container max-w-screen-xl mx-auto lg:pt-7 pt-16 h-fit px-3 ">
           <div>
+            {/* key features */}
             <div className=" lg:grid lg:grid-cols-5 lg:gap-5 py-7 ">
               <div className=" flex justify-center items-center col-span-2 pb-5">
                 <img
@@ -145,10 +148,10 @@ const ProductDetails = () => {
         </div>
       </div>
       {/* description details */}
-      <div>
+      <div className=" pb-10 px-3">
         <div>
           <div className=" container mx-auto max-w-screen-xl my-5">
-            <ul className=" flex gap-3">
+            <ul className=" flex flex-wrap gap-3">
               <Link className=" inline-block py-2 hover:bg-primaryDark  hover:text-white px-6 bg-white shadow rounded">
                 <span className=" font-medium">Description</span>
               </Link>
@@ -159,6 +162,97 @@ const ProductDetails = () => {
                 <span className=" font-medium">Reviews(0)</span>
               </Link>
             </ul>
+          </div>
+          <div className=" container max-w-screen-xl bg-white px-5 py-7 rounded shadow">
+            <h3 className=" font-titleFont font-semibold text-xl">
+              Description
+            </h3>
+            <div className=" pt-7">
+              <h2 className=" lg:text-xl text-base font-semibold font-titleFont leading-tight">
+                {product.description.title_1}
+              </h2>
+              <p className=" lg:text-base text-sm font-bodyFont font-medium lg:mt-5 mt-2">
+                {product.description.desc_1}
+              </p>
+            </div>
+            <div className=" pt-4">
+              <h2 className=" lg:text-xl text-base font-semibold font-titleFont leading-tight">
+                {product.description.title_2}
+              </h2>
+              <p className=" lg:text-base text-sm font-bodyFont font-medium lg:mt-5 mt-2">
+                {product.description.desc_2}
+              </p>
+            </div>
+          </div>
+          <div className=" container max-w-screen-xl bg-white px-5 py-7 rounded shadow mt-5">
+            <h2 className=" lg:text-xl text-base font-semibold font-titleFont leading-tight">
+              {product.description.title_3}
+            </h2>
+            <p className=" lg:text-base text-sm font-bodyFont font-medium lg:mt-5 mt-2 ">
+              {product.description.desc_3}
+            </p>
+          </div>
+
+          <div className=" container max-w-screen-xl bg-white px-5 py-7 rounded shadow mt-5">
+            <div className=" flex flex-wrap gap-2 justify-between items-center border-b pb-7">
+              <div className=" ">
+                <h3 className=" text-xl font-titleFont font-semibold">
+                  Questions(0)
+                </h3>
+                <p className=" text-sm text-[#666666] mt-3">
+                  Have question about this product? Get specific details about
+                  this product from expert.
+                </p>
+              </div>
+              <div className="">
+                <button className=" lg:py-2 py-2 rounded-md border border-[#3749bb] font-bodyFont text-[#3749bb] font-semibold relative overflow-hidden px-6 hover:text-white group">
+                  <span className=" absolute inset-0 bg-gradient-to-t from-blue-700 to-blue-900 transition-all duration-300 ease-out transform -translate-y-full group-hover:translate-y-0"></span>
+                  <span className=" relative z-10">Ask Question</span>
+                </button>
+              </div>
+            </div>
+            <div className=" w-full">
+              <div className=" flex flex-col py-12 justify-center items-center">
+                <div className=" w-[88px] h-[88px] bg-[#3749BB0D] rounded-full flex justify-center items-center">
+                  <RiMessage2Fill className=" text-[#3749bb] text-4xl" />
+                </div>
+                <p className=" text-sm text-[#666666] mt-3 font-bodyFont">
+                  This product has no reviews yet. Be the first one to write a
+                  review.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className=" container max-w-screen-xl bg-white px-5 py-7 rounded shadow mt-5">
+            <div className=" flex flex-wrap gap-2 justify-between items-center border-b pb-7">
+              <div className=" ">
+                <h3 className=" text-xl font-titleFont font-semibold">
+                  Reviews (0)
+                </h3>
+                <p className=" text-sm text-[#666666] mt-3">
+                  There are no questions asked yet. Be the first one to ask a
+                  question.
+                </p>
+              </div>
+              <div className="">
+                <button className="lg:py-2 py-2 rounded-md border border-[#3749bb] font-bodyFont text-[#3749bb] font-semibold relative overflow-hidden px-6 hover:text-white group">
+                  <span className="absolute inset-0 bg-gradient-to-t from-blue-700 to-blue-900 transition-all duration-300 ease-out transform -translate-y-full group-hover:translate-y-0"></span>
+                  <span className=" relative z-10">Write a Review</span>
+                </button>
+              </div>
+            </div>
+            <div className=" w-full">
+              <div className=" flex flex-col py-12 justify-center items-center">
+                <div className=" w-[88px] h-[88px] bg-[#3749BB0D] rounded-full flex justify-center items-center">
+                  <MdAssignment className=" text-[#3749bb] text-4xl" />
+                </div>
+                <p className=" text-sm text-[#666666] mt-3 font-bodyFont">
+                  This product has no reviews yet. Be the first one to write a
+                  review.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
